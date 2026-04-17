e4b153bc25060b1b34b35724a2d9bca026d54d85
Sanskar Agarwal <sanskar.agarwal99@gmail.com>
2026-04-17 16:42:00 +1000

    Hid interim results, reduced plot sizes, added tables to reduce page count

--- merge_datasets.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-crash = pd.read_excel('dataset/nsw_road_crash_data_2020-2024_crash.xlsx')
-tu = pd.read_excel('dataset/nsw_road_crash_data_2020-2024_traffic_unit.xlsx')
-
-merged = pd.merge(tu, crash, on='Crash ID', how='left')
-
-merged.to_excel('dataset/nsw_road_crash_merged.xlsx', index=False)
-print('Done:', merged.shape)
